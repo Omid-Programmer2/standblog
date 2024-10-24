a6b8e2e2200a8c966863c78a416f7a79c7ddcc30
Omid <omidprogrammer2@gmail.com>
2024-10-24 18:17:42 +0330

    articles_list.html(blog.html) added to project and also show all article in header all articles finished

--- blog/urls.py
+++ blog/urls.py
@@ -6,7 +6,8 @@ urlpatterns = [
     # path('detail/<int:pk>', views.post_detail, name='article_detail'),
     # path('detail/<int:pk>/<int:number>', views.post_detail, name='article_detail'),
     # path('detail/<str:title>', views.post_detail, name='article_detail'),
-    path('detail/<slug:slug>', views.post_detail, name='article_detail'),
+    path('detail/<slug:slug>', views.article_detail, name='article_detail'),
     # path('detail/<str:slug>', views.post_detail, name='article_detail'),
+    path('list', views.article_list, name='article_list'),
 
 ]
--- blog/views.py
+++ blog/views.py
@@ -4,8 +4,15 @@ from blog.models import Article
 
 
 # def post_detail(request, title):
-def post_detail(request, slug):
+def article_detail(request, slug):
     # print(type(slug))
     # article = Article.objects.get(id=pk)
     article = get_object_or_404(Article, slug=slug)
     return render(request, "blog/article_detail.html", {"article": article})
+
+
+def article_list(request):
+    articles = Article.objects.all()
+    return render(request, "blog/articles_list.html", {"articles": articles})
+
+
